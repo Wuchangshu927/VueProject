00edff7ac1ba8ee3b1b53707fb2a5572785c5552
wcs <794100338@qq.com>
2020-04-14 23:30:31 +0800

    加入后端代码

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,25 +1,65 @@
 <template>
   <v-app>
-      <v-app-bar height="50" app>
-        <v-btn fab elevation="0"  size="20"
+      <v-app-bar
+              id="app-bar"
+              absolute
+              app
+
+              flat
+              height="60"
+      >
+        <v-btn class="mr-3"
+               elevation="1"
+               fab
                small
                @click="drawer=!drawer"
         >
-          <v-icon>mdi-menu</v-icon>
+          <v-icon v-if="value">mdi-menu</v-icon>
+          <v-icon v-else>
+            mdi-dots-vertical
+          </v-icon>
 
         </v-btn>
-        <v-toolbar-title >{{$route.name}}</v-toolbar-title>
+
+        <v-toolbar-title
+                class="hidden-sm-and-down font-weight-light"
+                v-text="$route.name"
+        />
+
 <!--        <v-avatar>{{$store.state.isLogin?'成功':$router.push('/login')}}</v-avatar>-->
         <v-spacer/>
-        <v-toolbar-title :class="['mdi',$store.state.connected?'mdi-lan-connect':'mdi-lan-disconnect']"><v-icon></v-icon></v-toolbar-title>
 
-        <v-avatar>{{$store.state.username}}</v-avatar>
-        <v-icon @click="logout" title="注销">mdi-logout</v-icon>
+        <div class="mx-3" />
+        <v-btn
+                class="ml-2"
+                min-width="0"
+                text
+                to="/home"
+        ><v-icon>mdi-view-dashboard</v-icon></v-btn>
+        <v-btn
+                class="ml-2"
+                min-width="0"
+                text
+                to="/home/user"
+        >
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+        <v-avatar style="border: 1px solid #cccccc" class="ml-2">{{$store.state.username}}</v-avatar>
+
+
+        <v-btn
+                class="ml-2"
+                min-width="0"
+                @click="logout"
+                title="注销"
+        >
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+
+
       </v-app-bar>
     <v-navigation-drawer  v-model="drawer" width="200" mobile-break-point="640" app>
-
       <v-list-item>
-
         <v-list-item-content>
 
           <v-list-item-title class="title">
@@ -33,23 +73,43 @@
       </v-list-item>
 
       <v-divider></v-divider>
-
         <v-list>
-          <v-list-item to="/home/view">
-            <v-list-item-title >你好</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/home/user">
-            <v-list-item-title>gfdf g</v-list-item-title>
+          <v-list-item v-for="item,i in menus"
+                       :key="i"
+                       :to=item.to>
+            <v-avatar :class="['mdi',item.icon]"></v-avatar>
+            <v-list-item-title >{{item.title}}</v-list-item-title>
           </v-list-item>
         </v-list>
+
+
+        <template v-slot:append>
+          <v-list-item
+            to=""
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>文档/更多</v-list-item-title>
+
+
+          </v-list-item>
+        </template>
+
+
+
+
+
     </v-navigation-drawer>
     <v-content>
       <router-view/>
-    </v-content>
-    <v-footer class="">
-      <v-spacer>g</v-spacer>
 
-    </v-footer>
+      <v-footer absolute class="">
+
+        footer
+      </v-footer>
+    </v-content>
+
   </v-app>
 </template>
 
@@ -60,7 +120,17 @@ export default {
   name: 'Home',
   data(){
     return{
-    drawer:true
+    drawer:true,
+      menus:[{
+      title:"主页",
+        to:'/home',
+        icon:'mdi-home'
+      },
+      {
+        title:"页面",
+        to:'/home/view',
+        icon:'mdi-home'
+      }]
   }}
   ,methods:{
     logout(){
@@ -73,3 +143,11 @@ export default {
   }
 }
 </script>
+
+<style>
+
+  #app-bar{
+    border-bottom: 1px solid #cccccc; ;
+  }
+
+</style>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -10,11 +10,11 @@
                 <v-form class="loginContainer">
                     <v-text-field label="用户名" v-model="formData.username"></v-text-field>
                     <v-text-field label="密码" v-model="formData.password" type="password"></v-text-field>
+                    <v-text-field label="确认密码" v-model="formData.password2" type="password"></v-text-field>
                     <v-card-text class="d-inline-block">
 
                         <v-btn @click="register">注册</v-btn>
                         <v-btn @click="toLogin">返回登录</v-btn>
-                        <v-btn @click="getName">组价匿名</v-btn>
 
                     </v-card-text>
                 </v-form>
@@ -30,48 +30,23 @@
         data(){
             return{
                 formData:{
-                    username:'wcs',
-                    password:'123'
+                    username:'',
+                    password:'',
+                    password2:''
                 }
             }
         },
         methods:{
-            login(){
-                let vm=this
-                if (this.formData.username&&this.formData.password){
-                    this.$axios.post('users/register',{formData:this.formData})
-                        .then(response=>{
-                            console.log(response.data)
-
-
-                        })
-
-
-
-                }
-
-            },
             register(){
                 if (this.formData.username&&this.formData.password){
-                    this.$axios.post('users/register',{formdata:this.formData})
+                    this.postRequest('users/register',{formdata:this.formData})
                         .then(response=>{
                         console.log(response.data)
-                        })
-                        .catch(error=>{
-                            console.log('服务器内部错误！')
                         })
 
                     this.$store.commit('setUsername',this.formData.username)
                     // this.$router.push('/home')
                 }
-
-            },
-            getsession(){
-                let user=window.sessionStorage.getItem('user')
-                console.log(user)
-            },
-            getName(){
-                console.log(this.$route.name)
             },
             toLogin(){
                 this.$router.push('/login')
